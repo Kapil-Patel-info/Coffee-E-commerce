6a3bcac069fdc966ff0db513caaf181eba9d8f64
thekapilpatel <thekapilpatelofficial@gmail.com>
2025-06-27 07:22:45 +0530

    27 june 2025 starting project  making basic design and basic code setup

---
+++ Client/src/pages/userDashboard.jsx
@@ -0,0 +1,18 @@
+
+
+const userDashboard =()=>{
+    return(
+
+        <>
+        
+        
+        
+        
+        </>
+    );
+}
+
+
+export default userDashboard;
+ 
+
---
+++ Server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+let port = 8080;
+
+
+
+app.listen(port,()=>{
+console.log(`http://localhost:${port}`);
+});
+
+
+
